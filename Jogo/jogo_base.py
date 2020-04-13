c1d372b314034c671ae6c06caa06eb640f194397
mateusprov01 <mateussprovensidev@users.noreply.github.com>
2020-04-13 16:08:50 -0300

    Mudei Tudo
    Tive de mudar toda a forma de jogar, pois estava incorreto a forma de adicionar, coluna estava antes e depois a linha, ou seja, só funciona em matrizes quadradas

---
+++ Jogo/jogo_base.py
@@ -0,0 +1,124 @@
+from random import randint
+from time import sleep
+from os import system
+
+continuar = True
+reiniciar_loop = True
+
+
+def limpa_tela():
+    system('cls')
+
+
+def definir_porta_avioes():
+    linha_porta_avioes = randint(0, 4)
+    coluna_porta_avioes = randint(0, 9)
+    for i in range(5):
+        tabuleiro_back[linha_porta_avioes + i][coluna_porta_avioes] = 'P'
+
+
+def definir_botes():
+    for i in range(5):
+        linha_bote_atual = randint(0, 9)
+        coluna_bote_atual = randint(0, 9)
+        while tabuleiro_back[linha_bote_atual][coluna_bote_atual] == 'P' or \
+                tabuleiro_back[linha_bote_atual][coluna_bote_atual] == 'S' or \
+                tabuleiro_back[linha_bote_atual][coluna_bote_atual] == 'N' or \
+                tabuleiro_back[linha_bote_atual][coluna_bote_atual] == 'C':
+            linha_bote_atual = randint(0, 9)
+            coluna_bote_atual = randint(0, 9)
+        tabuleiro_back[linha_bote_atual][coluna_bote_atual] = 'B'
+
+
+def definir_submarino():
+    global reiniciar_loop
+    while True:
+        linha_submarino = randint(0, 7)
+        coluna_submarino = randint(0, 9)
+        for i in range(3):
+            if tabuleiro_back[linha_submarino + i][coluna_submarino] == 'P' or \
+                    tabuleiro_back[linha_submarino + i][coluna_submarino] == 'B' or \
+                    tabuleiro_back[linha_submarino + i][coluna_submarino] == 'N' or \
+                    tabuleiro_back[linha_submarino + i][coluna_submarino] == 'C':
+                reiniciar_loop = True
+                break
+            elif i == 2:
+                reiniciar_loop = False
+                break
+        if reiniciar_loop:
+            continue
+        else:
+            for i in range(3):
+                tabuleiro_back[linha_submarino + i][coluna_submarino] = 'S'
+            break
+
+
+def definir_navio():
+    global reiniciar_loop
+    while True:
+        linha_navio = randint(0, 9)
+        coluna_navio = randint(0, 6)
+        for i in range(4):
+            if tabuleiro_back[linha_navio][coluna_navio + i] == 'P' or \
+                    tabuleiro_back[linha_navio][coluna_navio + i] == 'S' or \
+                    tabuleiro_back[linha_navio][coluna_navio + i] == 'B' or \
+                    tabuleiro_back[linha_navio][coluna_navio + i] == 'N':
+                reiniciar_loop = True
+                break
+            elif i == 3:
+                reiniciar_loop = False
+                break
+        if reiniciar_loop:
+            continue
+        else:
+            for i in range(4):
+                tabuleiro_back[linha_navio][coluna_navio + i] = 'N'
+            break
+
+
+def definir_cargueiro():
+    global reiniciar_loop
+    while True:
+        linha_cargueiro = randint(0, 9)
+        coluna_cargueiro = randint(0, 4)
+        for i in range(6):
+            if tabuleiro_back[linha_cargueiro][coluna_cargueiro + i] == 'P' or \
+                    tabuleiro_back[linha_cargueiro][coluna_cargueiro + i] == 'S' or \
+                    tabuleiro_back[linha_cargueiro][coluna_cargueiro + i] == 'B' or \
+                    tabuleiro_back[linha_cargueiro][coluna_cargueiro + i] == 'N':
+                reiniciar_loop = True
+                break
+            elif i == 5:
+                reiniciar_loop = False
+                break
+        if reiniciar_loop:
+            continue
+        else:
+            for i in range(6):
+                tabuleiro_back[linha_cargueiro][coluna_cargueiro + i] = 'C'
+            break
+
+
+tabuleiro_back = []
+tabuleiro_usuario = []
+
+for linha in range(10):
+    tabuleiro_back.append([])
+    tabuleiro_usuario.append([])
+    for coluna in range(10):
+        tabuleiro_back[linha].append(';')
+        tabuleiro_usuario[linha].append('#')
+
+definir_porta_avioes()
+definir_botes()
+definir_submarino()
+
+definir_navio()
+
+definir_cargueiro()
+
+for l in range(10):
+    for c in range(10):
+        print(tabuleiro_back[l][c], end=' ')
+    print()
+print('-=' * 25)

092705c611d864c9b71261383d0ff0eb37139caa
mateusprov01 <mateussprovensidev@users.noreply.github.com>
2020-04-13 18:29:14 -0300

    Novas funçoes
    Diminui o codigo principal em diversas funções e acrescentei o continuar a jogar

--- Jogo/jogo_base.py
+++ Jogo/jogo_base.py
@@ -2,8 +2,10 @@ from random import randint
 from time import sleep
 from os import system
 
-continuar = True
+continuar_a_jogar = ''
 reiniciar_loop = True
+continuar_obrigatoriamente = linha_usuario = coluna_usuario = 0
+tipos_barcos = ('P', 'S', 'B', 'N')
 
 
 def limpa_tela():
@@ -11,17 +13,30 @@ def limpa_tela():
 
 
 def definir_porta_avioes():
-    linha_porta_avioes = randint(0, 4)
-    coluna_porta_avioes = randint(0, 9)
-    for i in range(5):
-        tabuleiro_back[linha_porta_avioes + i][coluna_porta_avioes] = 'P'
+    global reiniciar_loop
+    while True:
+        linha_porta_avioes = randint(0, 4)
+        coluna_porta_avioes = randint(0, 9)
+        for i in range(5):
+            if tabuleiro_back[linha_porta_avioes + i][coluna_porta_avioes] in tipos_barcos:
+                reiniciar_loop = True
+                break
+            elif i == 4:
+                reiniciar_loop = False
+                break
+        if reiniciar_loop:
+            continue
+        else:
+            for i in range(3):
+                tabuleiro_back[linha_porta_avioes + i][coluna_porta_avioes] = 'P'
+            break
 
 
 def definir_botes():
     for i in range(5):
         linha_bote_atual = randint(0, 9)
         coluna_bote_atual = randint(0, 9)
-        while tabuleiro_back[linha_bote_atual][coluna_bote_atual] in ('P', 'S', 'B', 'N'):
+        while tabuleiro_back[linha_bote_atual][coluna_bote_atual] in tipos_barcos:
             linha_bote_atual = randint(0, 9)
             coluna_bote_atual = randint(0, 9)
         tabuleiro_back[linha_bote_atual][coluna_bote_atual] = 'B'
@@ -33,7 +48,7 @@ def definir_submarino():
         linha_submarino = randint(0, 7)
         coluna_submarino = randint(0, 9)
         for i in range(3):
-            if tabuleiro_back[linha_submarino + i][coluna_submarino] in ('P', 'S', 'B', 'N'):
+            if tabuleiro_back[linha_submarino + i][coluna_submarino] in tipos_barcos:
                 reiniciar_loop = True
                 break
             elif i == 2:
@@ -53,7 +68,7 @@ def definir_navio():
         linha_navio = randint(0, 9)
         coluna_navio = randint(0, 6)
         for i in range(4):
-            if tabuleiro_back[linha_navio][coluna_navio + i] in ('P', 'S', 'B', 'N'):
+            if tabuleiro_back[linha_navio][coluna_navio + i] in tipos_barcos:
                 reiniciar_loop = True
                 break
             elif i == 3:
@@ -73,7 +88,7 @@ def definir_cargueiro():
         linha_cargueiro = randint(0, 9)
         coluna_cargueiro = randint(0, 4)
         for i in range(6):
-            if tabuleiro_back[linha_cargueiro][coluna_cargueiro + i] in ('P', 'S', 'B', 'N'):
+            if tabuleiro_back[linha_cargueiro][coluna_cargueiro + i] in tipos_barcos:
                 reiniciar_loop = True
                 break
             elif i == 5:
@@ -87,26 +102,122 @@ def definir_cargueiro():
             break
 
 
-tabuleiro_back = []
-tabuleiro_usuario = []
+def mostra_tabuleiro_no_teste():
+    for linha_teste in range(10):
+        for coluna_teste in range(10):
+            print(tabuleiro_back[linha_teste][coluna_teste], end=' ')
+        print()
 
-for linha in range(10):
-    tabuleiro_back.append([])
-    tabuleiro_usuario.append([])
-    for coluna in range(10):
-        tabuleiro_back[linha].append(';')
-        tabuleiro_usuario[linha].append('#')
 
-definir_porta_avioes()
-definir_botes()
-definir_submarino()
+def mostra_tabuleiro_usuario():
+    for linha_mostrando_usuario in range(10):
+        for coluna_mostrando_usuario in range(10):
+            print(tabuleiro_usuario[linha_mostrando_usuario][coluna_mostrando_usuario], end=' ')
+        print()
 
-definir_navio()
 
-definir_cargueiro()
+def receber_verificar_jogada_usuario():
+    global linha_usuario, coluna_usuario
+    while True:
+        while True:
+            try:
+                linha_usuario = int(input('\nDigite a linha, começa em 1: ')) - 1
+            except ValueError:
+                print('Digite um número inteiro, por favor.')
+                continue
+            if linha_usuario > 10 or linha_usuario < 1:
+                print('Digite uma linha válida.')
+            else:
+                break
+        while True:
+            try:
+                coluna_usuario = int(input('\nDigite a coluna, começa em 1: ')) - 1
+            except ValueError:
+                print('Digite um número inteiro, por favor.')
+                continue
+            if coluna_usuario > 10 or coluna_usuario < 1:
+                print('Digite uma coluna válida.')
+            else:
+                break
+        if tabuleiro_back[linha_usuario][coluna_usuario] in ('█', 'X'):
+            print('Você já jogou neste lugar, escolha outro.')
+            continue
+        else:
+            break
 
-for l in range(10):
-    for c in range(10):
-        print(tabuleiro_back[l][c], end=' ')
-    print()
-print('-=' * 25)
+
+def verificar_se_acertou():
+    global continuar_obrigatoriamente
+    if tabuleiro_back[linha_usuario][coluna_usuario] in tipos_barcos:
+        if tabuleiro_back[linha_usuario][coluna_usuario] == 'P':
+            print('BOOOOM. Acertou um porta aviões!')
+        elif tabuleiro_back[linha_usuario][coluna_usuario] == 'N':
+            print('BOOOOM. Acertou um návio!')
+        elif tabuleiro_back[linha_usuario][coluna_usuario] == 'B':
+            print('BOOOOM. Acertou um bote!')
+        elif tabuleiro_back[linha_usuario][coluna_usuario] == 'S':
+            print('BOOOOM. Acertou um submarino!')
+        sleep(2)
+        tabuleiro_back[linha_usuario][coluna_usuario] = 'X'
+        tabuleiro_usuario[linha_usuario][coluna_usuario] = 'X'
+    else:
+        print('BOOOOM... na água.')
+        tabuleiro_usuario[linha_usuario][coluna_usuario] = '█'
+        tabuleiro_back[linha_usuario][coluna_usuario] = '█'
+        sleep(2)
+    for linha_verificacao_vitoria in range(10):
+        for coluna_verificacao_vitoria in range(10):
+            if tabuleiro_back[linha_verificacao_vitoria][coluna_verificacao_vitoria] in tipos_barcos:
+                continuar_obrigatoriamente += 1
+                break
+    print('Parabéns, você ganhouu!!')
+
+
+def continuar_jogar():
+    global continuar_a_jogar
+    continuar_a_jogar = input('Deseja continuar jogando[S/N]? ').strip().upper()
+    while continuar_a_jogar not in ('S', 'N'):
+        print('Somente S ou N.')
+        continuar_a_jogar = input('\nDeseja continuar jogando[S/N]? ').strip().upper()
+    if continuar_a_jogar == 'S':
+        continuar_a_jogar = True
+    else:
+        continuar_a_jogar = False
+
+
+while True:
+    limpa_tela()
+    tabuleiro_back = []
+    tabuleiro_usuario = []
+
+    for linha in range(10):
+        tabuleiro_back.append([])
+        tabuleiro_usuario.append([])
+        for coluna in range(10):
+            tabuleiro_back[linha].append('A')
+            tabuleiro_usuario[linha].append('#')
+
+    definir_porta_avioes()
+    definir_botes()
+    definir_submarino()
+    definir_navio()
+    definir_cargueiro()
+
+    while True:
+        mostra_tabuleiro_no_teste()
+        print('-=' * 25)
+        mostra_tabuleiro_usuario()
+        print('-=' * 25)
+        receber_verificar_jogada_usuario()
+        print()
+        verificar_se_acertou()
+        if continuar_obrigatoriamente:
+            continue
+        else:
+            continuar_jogar()
+    if continuar_a_jogar:
+        print('\nQue bom que jogará mais!!! Pressione enter para continuar.')
+        input()
+    else:
+        print('\nPor que nos abandonaste... :(')
+        sleep(2)
